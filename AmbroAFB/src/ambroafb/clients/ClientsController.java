f7bbcad36a1c5c060717e9b5e2d56cca81389820
toka16 <tabra12@freeuni.edu.ge>
2016-03-15 12:52:08 +0400

    წავშალე დაკომნტარებული კოდი

--- AmbroAFB/src/ambroafb/clients/ClientsController.java
+++ AmbroAFB/src/ambroafb/clients/ClientsController.java
@@ -5,24 +5,13 @@
  */
 package ambroafb.clients;
 
-import ambroafb.AmbroAFB;
 import ambroafb.clients.viewadd.client_dialog.ClientDialog;
-import ambroafb.general.AlertMessage;
-import ambroafb.general.GeneralConfig;
-import ambroafb.general.Names;
-import ambroafb.general.Utils;
-import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -42,11 +31,6 @@ public class ClientsController implements Initializable {
         ClientDialog dialog = new ClientDialog(client);
         dialog.setDisabled();
         dialog.showAndWait();
-//        try{
-//            Stage stage = Utils.createStage("/ambroafb/clients/viewadd/ViewClient.fxml", GeneralConfig.getInstance().getTitleFor("view_client"), "/images/innerLogo.png", AmbroAFB.mainStage);
-//            stage.setResizable(false);
-//            stage.show();
-//        } catch(IOException ex){ AlertMessage alert = new AlertMessage(Alert.AlertType.ERROR, ex, Names.ERROR_MAIN_CONFIGURATION); alert.showAlert();}
     }
     
     @FXML 
@@ -60,15 +44,6 @@ public class ClientsController implements Initializable {
         }else{
             System.out.println("changed client: "+dialog.getResult());
         }
-//        System.out.println("selected: " + table.getSelectionModel().getSelectedItem().clientId);
-//        HashMap<String, Object> parameters = new HashMap<>();
-//        parameters.put("client", table.getSelectionModel().getSelectedItem());
-//        try{
-//            Stage stage = Utils.createStage("/ambroafb/clients/viewadd/EditClient.fxml", parameters, GeneralConfig.getInstance().getTitleFor("edit_client"), "/images/innerLogo.png", AmbroAFB.mainStage);
-//            stage.setResizable(false);
-//            stage.show();
-//            System.out.println("stage.getScene().lookup: " + stage.getScene().lookup("client"));
-//        } catch(IOException ex){ AlertMessage alert = new AlertMessage(Alert.AlertType.ERROR, ex, Names.ERROR_MAIN_CONFIGURATION); alert.showAlert();}
     }
         
     @FXML 
@@ -80,11 +55,6 @@ public class ClientsController implements Initializable {
         }else{
             System.out.println("changed client: "+dialog.getResult());
         }
-//        try{
-//            Stage stage = Utils.createStage("/ambroafb/clients/viewadd/AddClient.fxml", GeneralConfig.getInstance().getTitleFor("add_client"), "/images/innerLogo.png", AmbroAFB.mainStage);
-//            stage.setResizable(false);
-//            stage.show();
-//        } catch(IOException ex){ AlertMessage alert = new AlertMessage(Alert.AlertType.ERROR, ex, Names.ERROR_MAIN_CONFIGURATION); alert.showAlert();}
     }
     
     @FXML 
