788c841aedbf9e30ef2daaf5264b117d8afbf8c3
dato-Qobuladze <davidkobuladzemail@gmail.com>
2016-05-23 16:00:58 +0400

    კლიენტების ფილტრი

--- AmbroAFB/src/ambroafb/clients/ClientsController.java
+++ AmbroAFB/src/ambroafb/clients/ClientsController.java
@@ -7,10 +7,19 @@ package ambroafb.clients;
 
 import ambroafb.general.editor_panel.EditorPanelController;
 import java.net.URL;
+import java.util.Collections;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -21,6 +30,9 @@ public class ClientsController implements Initializable {
 
     @FXML
     private TableView<Client> table;
+    
+    @FXML
+    private TextField filter;
 
     @FXML
     private EditorPanelController editorPanelController;
@@ -40,8 +52,63 @@ public class ClientsController implements Initializable {
 
     //შეიძლება გატანა მშობელ კლასში
     public void assignTable() {
+        ObservableList<Client> clients = FXCollections.observableArrayList();
         Client.getClients().stream().forEach((client) -> {
-            table.getItems().add(client);
+            clients.add(client);
         });
+        
+        FilteredList<Client> filterData = new FilteredList<>(clients, p -> true);
+        FilterFieldChangeListener filtrListener = new FilterFieldChangeListener(filterData);
+        filter.textProperty().addListener(filtrListener);
+        
+        SortedList<Client> sorterData = new SortedList<>(filterData);
+        table.setItems(sorterData);
+        
+//        Client.getClients().stream().forEach((client) -> {
+//            table.getItems().add(client);
+//        });
+    }
+    
+    
+    private class FilterFieldChangeListener implements ChangeListener<String> {
+        
+        private FilteredList<Client> clientsData;
+        
+        public FilterFieldChangeListener(FilteredList<Client> filterData){
+            clientsData = filterData;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+            PredicateListener predListener = new PredicateListener(newValue);
+            clientsData.setPredicate(predListener);
+        }
+        
+    }
+    
+    
+    private class PredicateListener implements Predicate<Client> {
+        
+        private boolean predicate;
+        private String searchStr;
+        
+        public PredicateListener(String searchable){
+            searchStr = searchable.toLowerCase();
+            predicate = false;
+        }
+        
+        @Override
+        public boolean test(Client c) {
+            if (c == null || searchStr.isEmpty())
+                predicate = true;
+            else {
+                String firstName = c.getFirstName().toLowerCase();
+                String lastName = c.getLastName().toLowerCase();
+
+                predicate = firstName.contains(searchStr) || lastName.contains(searchStr);
+            }
+            return predicate;
+        }
+        
     }
 }
