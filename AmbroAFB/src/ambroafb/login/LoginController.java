121d3d8cc1c153026bcc24bef58f48427357909e
toka16 <tabra12@freeuni.edu.ge>
2016-06-27 18:10:21 +0400

    ლოჯინის ფანჯარა

--- AmbroAFB/src/ambroafb/login/LoginController.java
+++ AmbroAFB/src/ambroafb/login/LoginController.java
@@ -5,6 +5,8 @@
  */
 package ambroafb.login;
 
+import ambroafb.AmbroAFB;
+import ambroafb.general.AlertMessage;
 import ambroafb.general.GeneralConfig;
 import ambroafb.general.KFZClient;
 import ambroafb.general.Utils;
@@ -13,14 +15,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.prefs.Preferences;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.controlsfx.control.textfield.TextFields;
+import org.controlsfx.control.MaskerPane;
 
 /**
  * FXML Controller class
@@ -29,18 +34,24 @@ import org.controlsfx.control.textfield.TextFields;
  */
 public class LoginController extends Stage implements Initializable {
 
+    private static final String PREFS_USERNAME = "kfz_saved_username";
+
     @FXML
     private TextField username;
     @FXML
     private PasswordField password;
     @FXML
     private Label status;
+    @FXML
+    private MaskerPane masker;
 
     private boolean loggedIn;
+    private final Preferences prefs;
 
     public LoginController() {
         super();
 
+        prefs = Preferences.systemNodeForPackage(AmbroAFB.class);
         Scene scene = Utils.createScene("/ambroafb/login/Login.fxml", this);
         this.setScene(scene);
     }
@@ -51,23 +62,38 @@ public class LoginController extends Stage implements Initializable {
     }
 
     public void login() {
-        try {
-            GeneralConfig.getInstance().getServerClient(username.getText(), password.getText());
-            loggedIn = true;
-            close();
-            return;
-        } catch (IOException ex) {
-            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (KFZClient.KFZServerException ex) {
-            if (ex.getStatusCode() == 401) {
-                status.setText(GeneralConfig.getInstance().getTitleFor("invalide_username_password"));
+        masker.setVisible(true);
+        new Thread(() -> {
+            try {
+                GeneralConfig.getInstance().getServerClient(username.getText(), password.getText());
+                loggedIn = true;
+                prefs.put(PREFS_USERNAME, username.getText());
+                Platform.runLater(() -> {
+                    masker.setVisible(false);
+                    close();
+                });
+                return;
+            } catch (IOException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+                Platform.runLater(() -> {
+                    masker.setVisible(false);
+                    new AlertMessage(Alert.AlertType.ERROR, ex, "Network Error").showAlert();
+                });
+            } catch (KFZClient.KFZServerException ex) {
+                if (ex.getStatusCode() == 401) {
+                    Platform.runLater(() -> {
+                        masker.setVisible(false);
+                        status.setText(GeneralConfig.getInstance().getTitleFor("invalide_username_password"));
+                    });
+                }
             }
-        }
-        loggedIn = false;
+            loggedIn = false;
+        }).start();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        username.setText(prefs.get(PREFS_USERNAME, ""));
     }
 
 }
