7cedb2b849ab45943b3ba14b8a4645f57950629b
dato-Qobuladze <davidkobuladzemail@gmail.com>
2017-02-21 18:22:17 +0400

    generalParams სია

--- AmbroAFB/src/ambroafb/params_general/ParamGeneral.java
+++ AmbroAFB/src/ambroafb/params_general/ParamGeneral.java
@@ -5,10 +5,14 @@
  */
 package ambroafb.params_general;
 
+import ambro.AView;
 import ambroafb.general.DBUtils;
 import ambroafb.general.interfaces.EditorPanelable;
 import authclient.db.ConditionBuilder;
 import java.util.ArrayList;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
 import org.json.JSONObject;
 
 /**
@@ -17,10 +21,24 @@ import org.json.JSONObject;
  */
 public class ParamGeneral extends EditorPanelable {
     
-    private static final String DB_VIEW_NAME = "process_general_param_whole";
+    private static final String DB_VIEW_NAME = "process_general_params";
+    
+    @AView.Column(title = "%clients", width = "100", cellFactory = ParamsCellFactory.class)
+    private int clientId;
+    
+    @AView.Column(title = "%buysells", width = "100", cellFactory = ParamsCellFactory.class)
+    private int buysellId;
+    
+    @AView.Column(title = "%subjects", width = "100", cellFactory = ParamsCellFactory.class)
+    private int subjectId;
+    
+    @AView.Column(title = "%param_type", width = "100")
+    private String paramType;
+    
+    @AView.Column(title = "%param", width = "100")
+    private String param;
     
     public ParamGeneral(){
-        
     }
     
     // DB methods:
@@ -32,31 +50,103 @@ public class ParamGeneral extends EditorPanelable {
     }
     
     
+    // Getters:
+    public int getClientId() {
+        return clientId;
+    }
+
+    public int getBuysell() {
+        return buysellId;
+    }
+
+    public int getSubject() {
+        return subjectId;
+    }
+
+    public String getParamType() {
+        return paramType;
+    }
+
+    public String getParam() {
+        return param;
+    }
+    
+    
+    // Setters:
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setBuysell(int bysellId) {
+        this.buysellId = bysellId;
+    }
+
+    public void setSubject(int subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public void setParamType(String paramType) {
+        this.paramType = paramType;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+    
     
 
     // Overrides:
     @Override
-    public EditorPanelable cloneWithoutID() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ParamGeneral cloneWithoutID() {
+        ParamGeneral clone = new ParamGeneral();
+        clone.copyFrom(this);
+        return clone;
     }
 
     @Override
-    public EditorPanelable cloneWithID() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ParamGeneral cloneWithID() {
+        ParamGeneral clone = cloneWithoutID();
+        clone.setRecId(this.getRecId());
+        return clone;
     }
 
     @Override
     public void copyFrom(EditorPanelable other) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ParamGeneral otherParamGeneral = (ParamGeneral)other;
+        setClientId(otherParamGeneral.getClientId());
+        setBuysell(otherParamGeneral.getBuysell());
+        setSubject(otherParamGeneral.getSubject());
+        setParamType(otherParamGeneral.getParamType());
+        setParam(otherParamGeneral.getParam());
     }
 
     @Override
     public boolean compares(EditorPanelable backup) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ParamGeneral other = (ParamGeneral)backup;
+        return  getClientId() == other.getClientId() &&
+                getBuysell() == other.getBuysell() &&
+                getSubject() == other.getSubject() &&
+                getParamType().equals(other.getParamType()) &&
+                getParam().equals(other.getParam());
     }
 
     @Override
     public String toStringForSearch() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return  getClientId() + " " + getBuysell() + " " + getSubject() + " " +
+                    getParamType() + " " + getParam();
     }
+    
+    
+    public static class ParamsCellFactory implements Callback<TableColumn<ParamGeneral, Integer>, TableCell<ParamGeneral, Integer>> {
+
+        @Override
+        public TableCell<ParamGeneral, Integer> call(TableColumn<ParamGeneral, Integer> param) {
+            return new TableCell<ParamGeneral, Integer>() {
+                @Override
+                public void updateItem(Integer id, boolean empty) {
+                    setText(empty ? null : (id == 0 ? "ALL" : "" + id));
+                }
+            };
+        }
+    } 
 }
