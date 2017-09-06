12b3dd49ef2218a42d3eabfc5cff75a5d01ff471
dato-qobuladze <davidkobuladzemail@gmail.com>
2017-09-06 18:44:08 +0400

    CustomDialog files

---
+++ AmbroAFB/src/ambroafb/docs/types/custom/dialog/CustomDialog.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ambroafb.docs.types.custom.dialog;
+
+/**
+ *
+ * @author dkobuladze
+ */
+public class CustomDialog {
+    
+}
