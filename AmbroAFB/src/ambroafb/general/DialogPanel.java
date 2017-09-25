135a71bb517f28c699387e944c3cb67043ff6849
dato-qobuladze <davidkobuladzemail@gmail.com>
2017-09-25 20:06:43 +0400

    Enter -> Tab (Part I)

--- AmbroAFB/src/ambroafb/general/DialogPanel.java
+++ AmbroAFB/src/ambroafb/general/DialogPanel.java
@@ -23,6 +23,17 @@ public class DialogPanel extends StackPane {
     public final Object BACKUP_VALUE_KEY = new Object();
 
     public DialogPanel() {
+        // გადასაკეთებელია login Controller_ისთვის
+//        this.addEventFilter(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+//            if (event.getCode().equals(KeyCode.ENTER)) {
+//                Node focusedNode = this.getScene().getFocusOwner();
+////                if (nodeParentNotEqual(focusedNode, "okayCancel")){
+//                    focusedNode.fireEvent(new KeyEvent(null, null, KeyEvent.KEY_PRESSED, "", "\t", KeyCode.TAB, false, false, false, false));
+//                    event.consume();
+////                }
+//            }
+//        });
+        
         getChildren().addListener((ListChangeListener.Change<? extends Node> c) -> {
             customizeChildren();
         });
@@ -34,41 +45,54 @@ public class DialogPanel extends StackPane {
                 .filter((t) -> !t.getProperties().containsKey(MARKED_NODE_KEY))
                 .forEach(this::customize);
     }
+    
+    private boolean nodeParentNotEqual(Node node, String parentId){
+        return node.getParent().getId() == null || !node.getParent().getId().equals(parentId);
+    }
 
     private void customize(Node node) {
-        node.getProperties().put(MARKED_NODE_KEY, null);
-        if (node.getClass().equals(TextField.class)) {
-            TextField field = (TextField) node;
-            field.focusedProperty().addListener((observable, oldValue, newValue) -> {
-                if (!oldValue && newValue) {
-                    node.getProperties().put(BACKUP_VALUE_KEY, field.getText());
-                }
-            });
-            field.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
-                if (event.getCode().equals(KeyCode.ESCAPE)) {
-                    String backupValue = (String) node.getProperties().get(BACKUP_VALUE_KEY);
-                    if (backupValue != null) {
-                        field.setText(backupValue);
-                        int index = field.getText().length() - 1;
-                        if (index >= 0) {
-                            field.positionCaret(index);
-                            field.selectAll();
+        if (nodeParentNotEqual(node, "okayCancel")){
+            node.getProperties().put(MARKED_NODE_KEY, null);
+            if (node.getClass().equals(TextField.class)) {
+                TextField field = (TextField) node;
+                field.focusedProperty().addListener((observable, oldValue, newValue) -> {
+                    if (!oldValue && newValue) {
+                        node.getProperties().put(BACKUP_VALUE_KEY, field.getText());
+                    }
+                });
+                field.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+                    if (event.getCode().equals(KeyCode.ESCAPE)) {
+                        String backupValue = (String) node.getProperties().get(BACKUP_VALUE_KEY);
+                        if (backupValue != null) {
+                            field.setText(backupValue);
+                            int index = field.getText().length() - 1;
+                            if (index >= 0) {
+                                field.positionCaret(index);
+                                field.selectAll();
+                            }
                         }
                     }
-                }
-            });
-        } else if (node.getClass().equals(Button.class)) {
-            node.addEventFilter(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
-                if (event.getCode().equals(KeyCode.SPACE)) {
-                    event.consume();
+                });
+            } else if (node.getClass().equals(Button.class)) {
+                node.addEventFilter(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+                    if (event.getCode().equals(KeyCode.SPACE)) {
+                        event.consume();
+                    }
+                });
+            }
+            node.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+                if (event.getCode().equals(KeyCode.ENTER)) {
+                    node.fireEvent(new KeyEvent(null, null, KeyEvent.KEY_PRESSED, "", "\t", KeyCode.TAB, false, false, false, false));
                 }
             });
         }
-        node.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
-            if (event.getCode().equals(KeyCode.ENTER)) {
-                node.fireEvent(new KeyEvent(null, null, KeyEvent.KEY_PRESSED, "", "\t", KeyCode.TAB, false, false, false, false));
-            }
-        });
+        else {
+            node.addEventFilter(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+                if (event.getCode().equals(KeyCode.ENTER)) {
+                    ((Button)node).getOnAction().handle(null);
+                }
+            });
+        }
     }
 
 }
