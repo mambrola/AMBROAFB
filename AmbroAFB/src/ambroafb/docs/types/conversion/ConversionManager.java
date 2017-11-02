ff6f49f361252ec1837ac6e655f6e8731d50fecd
dato-qobuladze <davidkobuladzemail@gmail.com>
2017-11-02 17:03:47 +0400

    Clean up conversion comment code

--- AmbroAFB/src/ambroafb/docs/types/conversion/ConversionManager.java
+++ AmbroAFB/src/ambroafb/docs/types/conversion/ConversionManager.java
@@ -19,62 +19,10 @@ import javafx.stage.Stage;
  */
 public class ConversionManager extends EditorPanelableManager {
 
-//    private final String DB_DELETE_PROCEDURE_NAME = "doc_delete";
-//    private final String DB_VIEW_NAME = "docs_whole";
-    
     public ConversionManager(){
         dataFetchProvider = new ConversionDataFetchProvider();
         dataChangeProvider = new ConversionDataChangeProvider();
     }
-    
-//    @Override
-//    public EditorPanelable getOneFromDB(int id) {
-//        return null;
-//        JSONObject params = new ConditionBuilder().where().orGroup().or("rec_id", "=", id).or("parent_rec_id", "=", id).closeGroup().condition().build();
-//        ArrayList<Doc> bouquet = DBUtils.getObjectsListFromDB(Doc.class, DB_VIEW_NAME, params);
-//        Conversion conversion = makeConversionFrom(bouquet);
-//        return conversion;
-//    }
-//    
-//    private Conversion makeConversionFrom(ArrayList<Doc> docs){
-//        Conversion result = new Conversion();
-//        Doc firstDoc = docs.get(0);
-//        Doc secondDoc = docs.get(1);
-//        result.setRecId(firstDoc.getRecId());
-//        result.setDocDate(firstDoc.getDocDate());
-//        result.setDocInDocDate(firstDoc.getDocInDocDate());
-//        result.descripProperty().set(firstDoc.getDescrip());
-//       
-//        
-//        result.setSellCurrency(firstDoc.getIso());
-//        result.setSellAccount(firstDoc.debitProperty().get());
-//        result.setSellAmount(firstDoc.getAmount());
-//        
-//        result.setBuyingCurrency(secondDoc.getIso());
-//        result.setBuyingAccount(secondDoc.creditProperty().get());
-//        result.setBuyingAmount(secondDoc.getAmount());
-//        return result;
-//    }
-//    
-//    @Override
-//    public ArrayList<Doc> saveOneToDB(EditorPanelable newDocComponent) {
-//        return DBUtils.saveConversionDoc((Conversion)newDocComponent);
-//    }
-//
-//    @Override
-//    public boolean deleteOneFromDB(int id) {
-//        return DBUtils.deleteObjectFromDB(DB_DELETE_PROCEDURE_NAME, id);
-//    }
-//
-//    @Override
-//    public void undo() {
-//    }
-//
-//    @Override
-//    public Dialogable getDocDialogFor(Stage owner, Names.EDITOR_BUTTON_TYPE type, EditorPanelable object) {
-//        ConversionDialog dialog = new ConversionDialog(object, type, owner);
-//        return dialog;
-//    }
 
     @Override
     public Dialogable getDialogFor(Stage owner, Names.EDITOR_BUTTON_TYPE type, EditorPanelable object) {
