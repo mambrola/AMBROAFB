294478e6ec65c0cd06347387d9d2281ae7798bd6
dato-qobuladze <davidkobuladzemail@gmail.com>
2017-11-16 15:43:45 +0400

    mcire

--- AmbroAFB/src/ambroafb/docs/types/utilities/payment/PaymentUtilityManager.java
+++ AmbroAFB/src/ambroafb/docs/types/utilities/payment/PaymentUtilityManager.java
@@ -19,59 +19,11 @@ import javafx.stage.Stage;
  */
 public class PaymentUtilityManager extends EditorPanelableManager {
     
-//    private final String DB_VIEW_NAME = "docs";
-//    private final String DB_MERCHANDISES_PROCEDURE_NAME = "utility_get_merchandises";
-//    private final String DB_DELETE_PROCEDURE_NAME = "doc_delete";
-//    private PaymentUtility transferUtility, transferUtilityBackup;
-
     public PaymentUtilityManager(){
         dataFetchProvider = new PaymentUtilityDataFetchProvider();
         dataChangeProvider = new PaymentUtilityDataChangeProvider();
     }
     
-//    @Override
-//    public EditorPanelable getOneFromDB(int id) {
-//        JSONObject params = new ConditionBuilder().where().and("rec_id", "=", id).condition().build();
-//        PaymentUtility paymentUtility = DBUtils.getObjectFromDB(PaymentUtility.class, DB_VIEW_NAME, params);
-//        paymentUtility.utilityProperty().set(getDocMerchandise(paymentUtility.getProcessId()));
-//        return paymentUtility;
-//    }
-//    
-//    private DocMerchandise getDocMerchandise(int merchandiseProcessId) {
-//        ArrayList<DocMerchandise> merchandises = DBUtils.getObjectsListFromDBProcedure(DocMerchandise.class, DB_MERCHANDISES_PROCEDURE_NAME);
-//        Optional<DocMerchandise> opt = merchandises.stream().filter((DocMerchandise dm) -> dm.getRecId() == merchandiseProcessId).findFirst();
-//        return (opt.isPresent()) ? opt.get() : null;
-//    }
-//
-//    @Override
-//    public ArrayList<Doc> saveOneToDB(EditorPanelable newDocComponent) {
-//        ArrayList<Doc> docs = new ArrayList<>();
-//        docs.add(DBUtils.savePaymentUtility((PaymentUtility) newDocComponent));
-//        return docs;
-//    }
-//
-//    @Override
-//    public boolean deleteOneFromDB(int id) {
-//        return DBUtils.deleteObjectFromDB(DB_DELETE_PROCEDURE_NAME, id);
-//    }
-//
-//    @Override
-//    public void undo() {
-//        
-//    }
-//
-//    @Override
-//    public Dialogable getDocDialogFor(Stage owner, EditorPanel.EDITOR_BUTTON_TYPE type, EditorPanelable object) {
-//        PaymentUtilityDialog tranferDialog = new PaymentUtilityDialog(object, type, owner);
-//        
-//        return tranferDialog;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "TransferUtilityManager";
-//    }
-
     @Override
     public Dialogable getDialogFor(Stage owner, EditorPanel.EDITOR_BUTTON_TYPE type, EditorPanelable object) {
         PaymentUtilityDialog dialog = new PaymentUtilityDialog(object, type, owner);
