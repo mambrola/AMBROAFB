8d33390d2f1dc68dfa8100365941b4db6c86e651
dato-qobuladze <davidkobuladzemail@gmail.com>
2017-12-01 14:35:27 +0400

    mcire

--- AmbroAFB/src/ambroafb/general/okay_cancel/DialogOkayCancelController.java
+++ AmbroAFB/src/ambroafb/general/okay_cancel/DialogOkayCancelController.java
@@ -5,27 +5,15 @@
  */
 package ambroafb.general.okay_cancel;
 
-import ambroafb.general.AlertMessage;
-import ambroafb.general.AnnotiationUtils;
-import ambroafb.general.GeneralConfig;
-import ambroafb.general.editor_panel.EditorPanel.EDITOR_BUTTON_TYPE;
-import static ambroafb.general.editor_panel.EditorPanel.EDITOR_BUTTON_TYPE.ADD;
-import static ambroafb.general.editor_panel.EditorPanel.EDITOR_BUTTON_TYPE.ADD_BY_SAMPLE;
-import static ambroafb.general.editor_panel.EditorPanel.EDITOR_BUTTON_TYPE.DELETE;
-import static ambroafb.general.editor_panel.EditorPanel.EDITOR_BUTTON_TYPE.EDIT;
-import static ambroafb.general.editor_panel.EditorPanel.EDITOR_BUTTON_TYPE.VIEW;
-import ambroafb.general.interfaces.Dialogable;
-import ambroafb.general.interfaces.UserInteractiveDialogStage;
+import ambroafb.general.interfaces.DialogCloseObserver;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -37,6 +25,8 @@ public class DialogOkayCancelController implements Initializable {
     @FXML
     private Button okay, cancel;
 
+    private final List<DialogCloseObserver> closeObservers = new ArrayList<>();
+    
     /**
      * Initializes the controller class.
      * @param url
@@ -45,11 +35,23 @@ public class DialogOkayCancelController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         okay.setOnAction((ActionEvent event) -> {
-            ((UserInteractiveDialogStage)okay.getScene().getWindow()).okayAction();
+            closeObservers.forEach((observer) -> observer.okayAction()); // Note: Observers may unregister after this iteration complete, but don't into okayAction() or cancelAction()
         });
         cancel.setOnAction((ActionEvent event) -> {
-            ((UserInteractiveDialogStage)cancel.getScene().getWindow()).cancelAction();
+            closeObservers.forEach((observer) -> observer.cancelAction()); // Note: Observers may unregister after this iteration complete, but don't into okayAction() or cancelAction()
         });
+    }
+    
+    public void registerObserver(DialogCloseObserver observer){
+        if (observer != null){
+            closeObservers.add(observer);
+        }
+    }
+    
+    public void removeObserver(DialogCloseObserver observer){
+        if (observer != null){
+            closeObservers.remove(observer);
+        }
     }
     
     public Button getOkayButton(){
@@ -68,82 +70,4 @@ public class DialogOkayCancelController implements Initializable {
         cancel.setVisible(visible);
     }
 
-    @Deprecated
-    public void setButtonsFeatures(EDITOR_BUTTON_TYPE type){
-        String alertText;
-        okay.setDisable(false);
-        cancel.setDisable(false);
-        switch (type) {
-            case DELETE:
-                okay.setText(GeneralConfig.getInstance().getTitleFor("delete"));
-                alertText = "You Realy want Delete this item?";
-                okay.setOnAction((ActionEvent event) -> {
-                    String stageName = ((Stage)okay.getScene().getWindow()).getTitle();
-                    if(new AlertMessage(((Stage) okay.getScene().getWindow()), Alert.AlertType.CONFIRMATION, alertText, stageName).showAndWait().get().equals(ButtonType.OK)){
-                        ((Stage) okay.getScene().getWindow()).close();
-                    }
-                });
-                cancel.setOnAction((ActionEvent event) -> {
-                    operationCanceled();
-                    ((Stage) okay.getScene().getWindow()).close();
-                });
-                break;
-            case EDIT:
-            case ADD:
-            case ADD_BY_SAMPLE:
-                okay.setText(GeneralConfig.getInstance().getTitleFor(type.equals(EDITOR_BUTTON_TYPE.ADD) ? "add" : "save"));
-                okay.setOnAction((ActionEvent event) -> {
-                    Scene currScene = okay.getScene();
-                    Object controller = currScene.getProperties().get("controller");
-                    boolean allRequiredFieldsAreValid = AnnotiationUtils.everyFieldContentIsValidFor(controller, type);
-                    if (allRequiredFieldsAreValid){
-                        
-                        changeClosePermissionForStage(true);
-                        ((Stage) okay.getScene().getWindow()).close();
-                    }
-                });
-                alertText = "Close without saving changes?";    
-                cancel.setOnAction((ActionEvent event) -> {
-                    boolean anyFieldWasChanged = ((UserInteractiveDialogStage)cancel.getScene().getWindow()).anyComponentChanged();
-                    if (anyFieldWasChanged) {
-                        String stageName = ((Stage)okay.getScene().getWindow()).getTitle();
-                        AlertMessage alert = new AlertMessage(((Stage) okay.getScene().getWindow()), Alert.AlertType.CONFIRMATION, alertText, stageName);
-                        alert.setOwner((Stage)okay.getScene().getWindow());
-                        ButtonType buttonType = alert.showAndWait().get();
-                        if (buttonType.equals(ButtonType.OK)){
-                            operationCanceled();
-                            changeClosePermissionForStage(true);
-                            ((Stage) okay.getScene().getWindow()).close();
-                        }
-                        else{
-                            changeClosePermissionForStage(false);
-                        }
-                    }else{ // This case is needed. If nothing change the real object must become null.
-                        operationCanceled();
-                        changeClosePermissionForStage(true);
-                        ((Stage) okay.getScene().getWindow()).close();
-                    }
-                });
-                break;
-            case VIEW:
-                okay.setOnAction((ActionEvent event) -> {
-                    operationCanceled();
-                    ((Stage) okay.getScene().getWindow()).close();
-                });
-                cancel.setOnAction((ActionEvent event) -> {
-                    operationCanceled();
-                    ((Stage) okay.getScene().getWindow()).close();
-                });
-                cancel.setVisible(false);
-        }
-    }
-    
-    private void operationCanceled(){
-        ((Dialogable)cancel.getScene().getWindow()).operationCanceled();
-    }
-
-    private void changeClosePermissionForStage(boolean value) {
-        ((UserInteractiveDialogStage)cancel.getScene().getWindow()).changePermissionForClose(value);
-    }
-    
 }
