fa37fb806ead0e3254e03191adfe47a5bbf307d7
dato-qobuladze <davidkobuladzemail@gmail.com>
2018-01-22 12:39:09 +0400

    IntegerSlider modification

--- AmbroAFB/src/ambroafb/general/scene_components/integer_slider/IntegerSlider.java
+++ AmbroAFB/src/ambroafb/general/scene_components/integer_slider/IntegerSlider.java
@@ -5,8 +5,10 @@
  */
 package ambroafb.general.scene_components.integer_slider;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Slider;
 
 /**
@@ -17,17 +19,32 @@ public class IntegerSlider extends Slider {
     
     private final IntegerProperty currentValue = new SimpleIntegerProperty();
     
+    private int blockValue = 1; // by default
+    
     public IntegerSlider(){
+//        slider.setMin(sliderMin);
+//        slider.setMax(sliderMax);
+//        slider.setValue(3 * blockValue);
+//        setMajorTickUnit(blockValue);
+        setMinorTickCount(0);
+        setShowTickMarks(true);
+        setSnapToTicks(true);
+        valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+            setValue((newValue.intValue() / blockValue) * blockValue);
+        });
         
+        currentValue.bind(Bindings.createIntegerBinding(() -> (int)(this.getValue() / blockValue), this.valueProperty()));
     }
     
-    public void setRange(int min, int max){
+    public void setRange(int min, int max, int counter){
         setMin(min);
         setMax(max);
+        blockValue = counter;
+        setMajorTickUnit(blockValue);
     }
     
     public void setDefault(int value){
-        
+        setValue(value);
     }
     
     public IntegerProperty currnetValueProperty(){
